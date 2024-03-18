1702d3f806399d90f718e2fe7ad4d9b9a495819b
jojoin <yangjie@jojoin.com>
2024-03-18 23:04:02 +0800

    up

--- language/en/index.js
+++ language/en/index.js
@@ -7,7 +7,7 @@ module.exports = {
         'Buy Hacash', 'Mining Hacash', 'Create Wallet', 'Explore Blocks', 'Learn Hacash', 'Join Hacash'
     ],
 
-    ttl1: "Community Driven Roadmap and Crowdfunding",
+    ttl1: "Hacash Crowdfunding Mission",
 
     abt: [
         `About`, `HacashDAO is comprised of individuals with a profound comprehension of the fundamental nature of money, acknowledging that Bitcoin might not represent the most ideal currency.We hold a strong conviction that Hacash is poised to become the future's predominant mainstream currency.`, `Our dedication lies in rectifying the sluggish progress caused by the absence of an official team and the equitable distribution of Hacash. By leveraging the collective strength of the Hacash community, we curate and furnish extensive information, unceasingly champion the value and innovation inherent in Hacash, and propel Hacash towards advancement.`,
